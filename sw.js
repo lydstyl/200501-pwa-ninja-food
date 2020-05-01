21d4a3d7ee37ca965b73323713f4135f611d04a9
Gabriel Brun <lydstyl@gmail.com>
2020-05-01 22:39:14 +0200

    caching assets

--- sw.js
+++ sw.js
@@ -1,14 +1,39 @@
+const staticCacheName = 'site-static'; // any name we want
+
+const assets = [
+  '/',
+  '/index.html',
+  '/js/app.js',
+  '/js/ui.js',
+  '/js/materialize.min.js',
+  '/css/styles.css',
+  '/css/materialize.min.css',
+  '/img/dish.png',
+  'https://fonts.googleapis.com/icon?family=Material+Icons',
+  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+];
+
 // install the service worker
 self.addEventListener('install', (evt) => {
-  console.log('service worker has been installed', evt);
+  // console.log('service worker has been installed', evt);
+
+  evt.waitUntil(
+    // open the cache if exist or create it
+    caches.open(staticCacheName).then((cache) => {
+      // cache.add or addAll
+      console.log('caching shell assets');
+
+      cache.addAll(assets);
+    })
+  );
 });
 
 // activate service worker
 self.addEventListener('activate', (evt) => {
-  console.log('service worker is activated', evt);
+  // console.log('service worker is activated', evt);
 });
 
 // fetch event
 self.addEventListener('fetch', (evt) => {
-  console.log('fetch event', evt);
+  // console.log('fetch event', evt);
 });
