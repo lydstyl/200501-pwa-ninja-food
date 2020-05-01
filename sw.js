33aff80d75301aff072d2cc212332ddb7ba84a62
Gabriel Brun <lydstyl@gmail.com>
2020-05-01 23:00:23 +0200

    Getting Cached Assets

--- sw.js
+++ sw.js
@@ -10,7 +10,11 @@ const assets = [
   '/css/materialize.min.css',
   '/img/dish.png',
   'https://fonts.googleapis.com/icon?family=Material+Icons',
-  'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+  'https://fonts.gstatic.com/s/materialicons/v50/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2',
+  'manifest.json',
+  '/img/icons/icon-144x144.png',
+  '/pages/about.html',
+  '/pages/contact.html',
 ];
 
 // install the service worker
@@ -36,4 +40,10 @@ self.addEventListener('activate', (evt) => {
 // fetch event
 self.addEventListener('fetch', (evt) => {
   // console.log('fetch event', evt);
+
+  evt.respondWith(
+    caches.match(evt.request).then((cacheRes) => {
+      return cacheRes || fetch(evt.request); // return the cacheRes but if empty return initial request
+    })
+  );
 });
